3f737d06662afb3ef7ca2c2e80a6ef2720227ee5
bilQubil <aqbilhafizh37@gmail.com>
2025-06-10 21:32:58 +0700

    feat/ add analytics n insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -32,6 +34,8 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <Providers>{children}</Providers>
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
